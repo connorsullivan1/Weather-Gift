d5dbe470fcc87af6ecd7f953e12ce7c729893107
Connor Sullivan <sulliamr@bc.edu>
2020-03-24 00:17:59 -0400

    7.12 WeatherDetail

---
+++ Weather-Gift2/WeatherDetail.swift
@@ -0,0 +1,67 @@
+//
+//  WeatherDetail.swift
+//  Weather-Gift2
+//
+//  Created by Connor on 3/23/20.
+//  Copyright © 2020 Connor Sullivan. All rights reserved.
+//
+
+import Foundation
+
+class WeatherDetail: WeatherLocation {
+    
+    struct Result: Codable {
+        var timezone: String
+        var currently: Currently
+        var daily: Daily
+    }
+    
+    struct Currently: Codable {
+        var temperature: Double
+    }
+    
+    struct Daily: Codable {
+        var summary: String
+    }
+    
+    var timezone = ""
+    var temperature = 0
+    var summary = ""
+    
+    
+    func getData(completed: @escaping () -> ()) {
+        let coordinates = "\(latitude),\(longitude)"
+        let urlString = "\(APIurls.darkSkyURL)\(APIkeys.darkSkykey)/\(coordinates)"
+        
+        print("we are accessing the URL \(urlString)")
+        //Create URL
+        guard let url = URL(string: urlString) else {
+            print("ERROR: COuldnt Not Create a URL from \(urlString)")
+            completed()
+            return
+        }
+        
+        //Create Session
+        let session = URLSession.shared
+        
+        // Get data with .dataTask Method
+        let task = session.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print("ERROR: \(error.localizedDescription)")
+            }
+            
+            
+            do{
+                let result = try JSONDecoder().decode(Result.self, from: data!)
+                self.timezone = result.timezone
+                self.temperature = Int(result.currently.temperature.rounded())
+                self.summary = result.daily.summary
+            } catch {
+                print("JSON ERROR:\(error.localizedDescription)")
+            }
+            completed()
+        }
+        task.resume()
+    }
+    
+}
