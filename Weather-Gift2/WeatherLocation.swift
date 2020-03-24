6bda7e114c78eed7ce3ee6b9332e38d77fdfce82
Connor Sullivan <sulliamr@bc.edu>
2020-03-23 20:40:14 -0400

    7.2

---
+++ Weather-Gift2/WeatherLocation.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherLocation.swift
+//  Weather-Gift
+//
+//  Created by Connor on 3/23/20.
+//  Copyright © 2020 Connor Sullivan. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name: String
+    var latitude: Double
+    var longitude: Double
+}
